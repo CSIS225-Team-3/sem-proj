43f87319ee361802cc4f485a7374f7b8a05a359d
lannucci5 <ld19annu@siena.edu>
2026-04-07 20:47:23 -0400

    Password Strength Checker

---
+++ Password_Strength_Checker/PasswordChecker.java
@@ -0,0 +1,11 @@
+package Password_Strength_Checker;
+import Password_Strength_Checker.*;
+
+/**
+ * Main for PasswordChecker, starts it.
+ */
+public class PasswordChecker {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new PasswordCheckerUI());
+    }
+}
---
+++ Password_Strength_Checker/PasswordCheckerUI.java
@@ -0,0 +1,55 @@
+package Password_Strength_Checker;
+import Password_Strength_Checker.*;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+/**
+ * User Interface for the password strength checker.
+ */
+public class PasswordCheckerUI implements Runnable, ActionListener {
+
+    private JTextField passwordBox;
+    private JLabel resultLabel;
+    private JButton checkButton;
+
+    @Override
+    public void run() {
+
+        JFrame frame = new JFrame("Password Strength Checker");
+        frame.setPreferredSize(new Dimension(400, 200));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        frame.add(mainPanel);
+
+        JLabel title = new JLabel("Password Strength Checker", JLabel.CENTER);
+        mainPanel.add(title, BorderLayout.NORTH);
+
+        JPanel centerPanel = new JPanel();
+        centerPanel.add(new JLabel("Enter Password:"));
+
+        passwordBox = new JTextField(20);
+        centerPanel.add(passwordBox);
+
+        checkButton = new JButton("Check");
+        checkButton.addActionListener(this);
+        centerPanel.add(checkButton);
+
+        mainPanel.add(centerPanel, BorderLayout.CENTER);
+
+        resultLabel = new JLabel("Strength: ", JLabel.CENTER);
+        mainPanel.add(resultLabel, BorderLayout.SOUTH);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        return;
+    }
+}
