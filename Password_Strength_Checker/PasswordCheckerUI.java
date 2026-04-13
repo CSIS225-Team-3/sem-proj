1eca1c75cf16b5b0cf5170a8fabcc7ca80d13c19
AhyaanMalik <ak27mali@siena.edu>
2026-04-13 08:13:17 -0400

    Added messages for differing strengths, and constants for different strength thresholds.

--- Password_Strength_Checker/PasswordCheckerUI.java
+++ Password_Strength_Checker/PasswordCheckerUI.java
@@ -11,11 +11,24 @@ import javax.swing.*;
  * User Interface for the password strength checker.
  */
 public class PasswordCheckerUI implements Runnable, ActionListener {
+
+    private String[] phrasesWeak;
+    private String[] phrasesMedium;
+    private String[] phrasesStrong;
+    private String[] phrasesVeryStrong;
+
+    private JLabel strengthMessage;
+
     private JTextField passwordBox;
     private JButton checkButton;
     private JProgressBar strengthBar;
 
     private int strength;
+
+
+    private static final int LOW_STRENGTH = 10;
+    private static final int MEDIUM_STRENGTH = 20;
+    private static final int HIGH_STRENGTH = 30;
 
     @Override
     public void run() {
@@ -47,6 +60,9 @@ public class PasswordCheckerUI implements Runnable, ActionListener {
         checkButton.addActionListener(this);
         centerPanel.add(checkButton);
 
+        strengthMessage = new JLabel(" ");
+        centerPanel.add(strengthMessage, BorderLayout.CENTER);
+
         mainPanel.add(centerPanel, BorderLayout.CENTER);
         // Center panel done
 
@@ -57,6 +73,8 @@ public class PasswordCheckerUI implements Runnable, ActionListener {
 
         frame.pack();
         frame.setVisible(true);
+
+        initPhrases();
     }
 
     @Override
@@ -64,9 +82,47 @@ public class PasswordCheckerUI implements Runnable, ActionListener {
         getAndUpdateScore();
     }
 
+    private void initPhrases() {
+        phrasesWeak = new String[] {
+                "Your password is very weak. Consider a stronger password.",
+                "Your password is weak. Consider changing your password.",
+                "Your password is weak. Consider adding more complexity."
+        };
+
+        phrasesMedium = new String[] {
+                "Your password is mildly weak. Consider a stronger password.",
+                "Your password is decent. Consider adding more complexity.",
+                "Your password is not bad, but could be stronger."
+        };
+
+        phrasesStrong = new String[] {
+                "Your password is good, but it could be stronger.",
+                "Your password is stronger than most, but it could be stronger.",
+                "Your password is great, but it could be even better."
+        };
+
+        phrasesVeryStrong = new String[] {
+                "Your password is very strong!",
+                "Your password is excellent!",
+                "Your password is amazing!"
+        };
+    }
+
+    private void setStrengthMessage() {
+        if (strength < LOW_STRENGTH)
+            strengthMessage.setText(phrasesWeak[(int) (Math.random() * phrasesWeak.length)]);
+        else if (strength < MEDIUM_STRENGTH)
+            strengthMessage.setText(phrasesMedium[(int) (Math.random() * phrasesMedium.length)]);
+        else if (strength < HIGH_STRENGTH)
+            strengthMessage.setText(phrasesStrong[(int) (Math.random() * phrasesStrong.length)]);
+        else
+            strengthMessage.setText(phrasesVeryStrong[(int) (Math.random() * phrasesVeryStrong.length)]);
+    }
+
     private void getAndUpdateScore() {
         calculateScore();
         setColor();
+        setStrengthMessage();
         strengthBar.setValue(strength);
         strengthBar.setString(String.valueOf(strength));
     }
@@ -77,35 +133,35 @@ public class PasswordCheckerUI implements Runnable, ActionListener {
         int choices = 0;
 
         if (hasUpperCase(password)) {
-            choices += 26; //26 uppercase letters
+            choices += 26; // 26 uppercase letters
         }
 
         if (hasLowerCase(password)) {
-            choices += 26; //26 lowercase letters
+            choices += 26; // 26 lowercase letters
         }
 
         if (hasNum(password)) {
-            choices += 10; //10 numbers
+            choices += 10; // 10 numbers
         }
 
         if (hasSymbol(password)) {
-            choices += 32; //~32 symbols
+            choices += 32; // ~32 symbols
         }
 
         // Length ^ options per character
         // double is required because the result quickly becomes extremely large
         double combinations = Math.pow(choices, password.length());
 
-        //Scale down the massive number
-        strength = (int)Math.log(combinations) / 2;
+        // Scale down the massive number
+        strength = (int) Math.log(combinations) / 2;
     }
 
     private void setColor() {
-        if (strength < 10)
+        if (strength < LOW_STRENGTH)
             strengthBar.setForeground(Color.RED);
-        else if (strength < 20)
+        else if (strength < MEDIUM_STRENGTH)
             strengthBar.setForeground(Color.ORANGE);
-        else if (strength < 30)
+        else if (strength < HIGH_STRENGTH)
             strengthBar.setForeground(new Color(220, 220, 0));
         else
             strengthBar.setForeground(new Color(0, 200, 0));
