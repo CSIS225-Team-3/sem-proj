c7a4af12b6d5f14d32fc11813aee2f646ff3546b
AhyaanMalik <ak27mali@siena.edu>
2026-04-13 08:30:01 -0400

    finished javadoc, changed Math.random to use Random class, changed depreciated leftover method to correct method

--- Password_Strength_Checker/PasswordCheckerUI.java
+++ Password_Strength_Checker/PasswordCheckerUI.java
@@ -6,32 +6,52 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Color;
 import javax.swing.*;
+import java.util.Random;
 
 /**
  * User Interface for the password strength checker.
  */
 public class PasswordCheckerUI implements Runnable, ActionListener {
 
+    /** Array of phrases for weak passwords */
     private String[] phrasesWeak;
+
+    /** Array of phrases for medium passwords */
     private String[] phrasesMedium;
+
+    /** Array of phrases for strong passwords */
     private String[] phrasesStrong;
+
+    /** Array of phrases for very strong passwords */
     private String[] phrasesVeryStrong;
 
+    /** Label for displaying password strength message */
     private JLabel strengthMessage;
 
+    /** Password input field */
     private JPasswordField passwordBox;
+
+    /** Checkbox for showing/hiding password */
     private JCheckBox showPassword;
 
+    /** Button for checking password strength */
     private JButton checkButton;
+
+    /** Progress bar for displaying password strength */
     private JProgressBar strengthBar;
 
+    /** The current password strength */
     private int strength;
 
+    /** Constants for password strength levels */
     private static final int LOW_STRENGTH = 10;
     private static final int MEDIUM_STRENGTH = 20;
     private static final int HIGH_STRENGTH = 30;
 
     @Override
+    /**
+     * Initializes the interface and sets up the components.
+     */
     public void run() {
         // Doesn't apply to existing JFrames, so must be called first
         JFrame.setDefaultLookAndFeelDecorated(true);
@@ -84,6 +104,11 @@ public class PasswordCheckerUI implements Runnable, ActionListener {
     }
 
     @Override
+    /**
+     * Handles action events
+     * 
+     * @param e the action event
+     */
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == showPassword) {
             togglePasswordVisibility();
@@ -92,14 +117,22 @@ public class PasswordCheckerUI implements Runnable, ActionListener {
         }
     }
 
+    /**
+     * Toggles password visability based on the state of the showPassword checkbox.
+     */
     private void togglePasswordVisibility() {
         if (showPassword.isSelected()) {
-            passwordBox.setEchoChar((char) 0); // Show password
+            // Show password
+            passwordBox.setEchoChar((char) 0);
         } else {
-            passwordBox.setEchoChar('*'); // Hide password
+            // Hide password
+            passwordBox.setEchoChar('*');
         }
     }
 
+    /**
+     * Initializes the arrays of phrases for different password strength levels.
+     */
     private void initPhrases() {
         phrasesWeak = new String[] {
                 "Your password is very weak. Consider a stronger password.",
@@ -126,17 +159,25 @@ public class PasswordCheckerUI implements Runnable, ActionListener {
         };
     }
 
+    /**
+     * Sets the strength message based on password strength randomly.
+     */
     private void setStrengthMessage() {
+        Random rand = new Random();
+
         if (strength < LOW_STRENGTH)
-            strengthMessage.setText(phrasesWeak[(int) (Math.random() * phrasesWeak.length)]);
+            strengthMessage.setText(phrasesWeak[rand.nextInt(0, phrasesWeak.length)]);
         else if (strength < MEDIUM_STRENGTH)
-            strengthMessage.setText(phrasesMedium[(int) (Math.random() * phrasesMedium.length)]);
+            strengthMessage.setText(phrasesMedium[rand.nextInt(0, phrasesMedium.length)]);
         else if (strength < HIGH_STRENGTH)
-            strengthMessage.setText(phrasesStrong[(int) (Math.random() * phrasesStrong.length)]);
+            strengthMessage.setText(phrasesStrong[rand.nextInt(0, phrasesStrong.length)]);
         else
-            strengthMessage.setText(phrasesVeryStrong[(int) (Math.random() * phrasesVeryStrong.length)]);
+            strengthMessage.setText(phrasesVeryStrong[rand.nextInt(0, phrasesVeryStrong.length)]);
     }
 
+    /**
+     * Calculates and updates the password strength score, color, message, and bar.
+     */
     private void getAndUpdateScore() {
         calculateScore();
         setColor();
@@ -145,8 +186,11 @@ public class PasswordCheckerUI implements Runnable, ActionListener {
         strengthBar.setString(String.valueOf(strength));
     }
 
+    /**
+     * Calculates password strength
+     */
     private void calculateScore() {
-        String password = passwordBox.getText();
+        String password = new String(passwordBox.getPassword());
 
         int choices = 0;
 
@@ -174,6 +218,9 @@ public class PasswordCheckerUI implements Runnable, ActionListener {
         strength = (int) Math.log(combinations) / 2;
     }
 
+    /**
+     * Sets color of the strength bar based on the password strength.
+     */
     private void setColor() {
         if (strength < LOW_STRENGTH)
             strengthBar.setForeground(Color.RED);
@@ -185,6 +232,12 @@ public class PasswordCheckerUI implements Runnable, ActionListener {
             strengthBar.setForeground(new Color(0, 200, 0));
     }
 
+    /**
+     * Check for uppercase
+     * 
+     * @param password the password to check
+     * @return true if the password contains an uppercase letter, false otherwise
+     */
     private boolean hasUpperCase(String password) {
         for (int i = 0; i < password.length(); i++)
             if (Character.isUpperCase(password.charAt(i)))
@@ -192,6 +245,12 @@ public class PasswordCheckerUI implements Runnable, ActionListener {
         return false;
     }
 
+    /**
+     * Check for lowercase
+     * 
+     * @param password the password to check
+     * @return true if the password contains a lowercase letter, false otherwise
+     */
     private boolean hasLowerCase(String password) {
         for (int i = 0; i < password.length(); i++)
             if (Character.isLowerCase(password.charAt(i)))
@@ -199,6 +258,12 @@ public class PasswordCheckerUI implements Runnable, ActionListener {
         return false;
     }
 
+    /**
+     * Check for numbers
+     * 
+     * @param password the password to check
+     * @return true if the password contains a number, false otherwise
+     */
     private boolean hasNum(String password) {
         for (int i = 0; i < password.length(); i++)
             if (Character.isDigit(password.charAt(i)))
@@ -206,6 +271,12 @@ public class PasswordCheckerUI implements Runnable, ActionListener {
         return false;
     }
 
+    /**
+     * Check for symbols
+     * 
+     * @param password the password to check
+     * @return true if the password contains a symbol, false otherwise
+     */
     private boolean hasSymbol(String password) {
         for (int i = 0; i < password.length(); i++)
             if (!Character.isDigit(password.charAt(i)) &&
