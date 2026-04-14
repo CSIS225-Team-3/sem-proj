607023d87fc25cc6dce94a51d7c2a79e07d5e9e6
AhyaanMalik <ak27mali@siena.edu>
2026-04-14 12:17:21 -0400

    very basic minesweeper template copied from my Concentration Project. There is no logic yet, and the colors will need changing.

---
+++ Minesweeper/MainMenu.java
@@ -0,0 +1,211 @@
+package Minesweeper;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.CardLayout;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.BorderFactory;
+
+/**
+ * Main Game to show the options for type of modes.
+ * 
+ * @author Ahyaan Malik (so far)
+ * @version 4/14/2026
+ */
+public class MainMenu extends JPanel implements ActionListener, Runnable {
+
+    /** The name of the menu card */
+    private static final String MENU_CARD = "Menu";
+
+    private static final String TWO_DIMENSIONS = "2D Minesweeper";
+
+    private static final String THREE_DIMENSIONS = "3D Minesweeper";
+
+    /** The name of the gamble mode card */
+    private static final String FOUR_DIMENSIONS = "4D Minesweeper";
+
+    private static final String HYPERBOLIC = "Hyperbolic Minesweeper";
+
+    /** The CardLayout for managing panels */
+    private CardLayout cardLayout;
+
+    /** The JPanel that holds the cards */
+    private JPanel cards;
+
+    /** The JFrame for the main window */
+    private JFrame frame;
+
+    /** The main text label for the menu */
+    private JLabel mainText;
+
+    /** The spinners for configuring the number of rows */
+    private JSpinner rowsSpinner;
+
+    /** The spinner for configuring the number of columns */
+    private JSpinner colsSpinner;
+
+    /** The label for displaying error messages */
+    private JLabel errorLabel;
+
+    private JButton twoDimension;
+
+    private JButton threeDimension;
+
+    private JButton fourDimension;
+
+    private JButton hyperbolic;
+
+    /** The primary color for the UI */
+    public final static Color PRIMARY_COLOR = Color.PINK;
+
+    /** The secondary color for the UI */
+    public final static Color SECONDARY_COLOR = new Color(207, 125, 151);
+
+    /** The tertiary color for the UI */
+    public final static Color TERTIARY_COLOR = new Color(255, 190, 200);
+
+    /**
+     * Constructor for the MainMenu class.
+     * 
+     * @param cardLayout the CardLayout used to manage the different mode panels
+     * @param cards      the JPanel that holds the different mode panels
+     */
+    public MainMenu(CardLayout cardLayout, JPanel cards) {
+        this.cardLayout = cardLayout;
+        this.cards = cards;
+
+        setLayout(new BorderLayout());
+    }
+
+    /**
+     * The run method to set up the GUI.
+     */
+    @Override
+    public void run() {
+        // Our basic GUI setup, a JFrame with a JPanel inside it.
+        frame = new JFrame("Minesweeper");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(800, 800));
+        frame.setResizable(false);
+
+        cardLayout = new CardLayout();
+        cards = new JPanel(cardLayout);
+
+        mainText = new JLabel("Welcome to Minesweeper!", SwingConstants.CENTER);
+        mainText.setFont(mainText.getFont().deriveFont(48.0f));
+        add(mainText, BorderLayout.NORTH);
+
+        JPanel centerPanel = new JPanel(new BorderLayout(0, 20));
+        centerPanel.setBorder(BorderFactory.createEmptyBorder(20, 40, 20, 40));
+
+        JPanel configPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 8));
+        configPanel.setBorder(BorderFactory.createTitledBorder("Game Settings"));
+
+        configPanel.add(new JLabel("Rows: "));
+        rowsSpinner = new JSpinner(new SpinnerNumberModel(9, 2, 40, 1));
+        rowsSpinner.setPreferredSize(new Dimension(60, 30));
+        configPanel.add(rowsSpinner);
+
+        configPanel.add(new JLabel("Columns: "));
+        colsSpinner = new JSpinner(new SpinnerNumberModel(9, 2, 40, 1));
+        colsSpinner.setPreferredSize(new Dimension(60, 30));
+        configPanel.add(colsSpinner);
+
+        centerPanel.add(configPanel, BorderLayout.NORTH);
+
+        JPanel errorPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        errorLabel = new JLabel(" ", SwingConstants.CENTER);
+        errorLabel.setForeground(Color.RED);
+        errorPanel.add(errorLabel);
+        centerPanel.add(errorPanel, BorderLayout.CENTER);
+
+        JPanel modesPanel = new JPanel(new GridLayout(6, 1, 0, 5));
+
+        twoDimension = new JButton("2D Minesweeper");
+        twoDimension.addActionListener(this);
+        threeDimension = new JButton("3D Minesweeper");
+        threeDimension.addActionListener(this);
+        fourDimension = new JButton("4D Minesweeper");
+        fourDimension.addActionListener(this);
+        hyperbolic = new JButton("Hyperbolic Minesweeper");
+        hyperbolic.addActionListener(this);
+
+        modesPanel.add(twoDimension);
+        modesPanel.add(threeDimension);
+        modesPanel.add(fourDimension);
+        modesPanel.add(hyperbolic);
+
+        centerPanel.add(modesPanel, BorderLayout.SOUTH);
+
+        add(centerPanel, BorderLayout.CENTER);
+
+        // CardLayout
+        cards.add(this, MENU_CARD);
+
+        cardLayout.show(cards, MENU_CARD);
+
+        centerPanel.setBackground(PRIMARY_COLOR);
+        modesPanel.setBackground(SECONDARY_COLOR);
+        configPanel.setBackground(SECONDARY_COLOR);
+        errorPanel.setBackground(PRIMARY_COLOR);
+
+        twoDimension.setBackground(TERTIARY_COLOR);
+        threeDimension.setBackground(TERTIARY_COLOR);
+        fourDimension.setBackground(TERTIARY_COLOR);
+        hyperbolic.setBackground(TERTIARY_COLOR);
+
+        rowsSpinner.setBackground(TERTIARY_COLOR);
+        colsSpinner.setBackground(TERTIARY_COLOR);
+
+        setBackground(PRIMARY_COLOR);
+
+        frame.add(cards);
+        // Display the window we've created.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    /**
+     * Listens for JButton actions and responds
+     * 
+     * @param e The JButton to listen for
+     */
+    public void actionPerformed(ActionEvent e) {
+
+        int rows = (int) rowsSpinner.getValue();
+        int cols = (int) colsSpinner.getValue();
+
+        String buttonText = e.getActionCommand();
+
+        if (buttonText.equals("2D Minesweeper")) {
+            cards.add(new MinesweeperTwoDimensions(rows, cols, cardLayout, cards), TWO_DIMENSIONS);
+            cardLayout.show(cards, TWO_DIMENSIONS);
+        } else if (buttonText.equals("3D Minesweeper")) {
+            System.out.println("3D Minesweeper coming soon!");
+        } else if (buttonText.equals("4D Minesweeper")) {
+            System.out.println("4D Minesweeper coming soon!");
+        } else if (buttonText.equals("Hyperbolic Minesweeper")) {
+            System.out.println("Hyperbolic Minesweeper coming soon!");
+        }
+    }
+
+    /**
+     * The main method is responsible for creating a thread
+     * that will construct and show the GUI.
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new MainMenu(null, null));
+    }
+}
---
+++ Minesweeper/MinesweeperTwoDimensions.java
@@ -0,0 +1,196 @@
+package Minesweeper;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * A proper game of concentration with a mxn grid of buttons
+ *
+ * @author Ahyaan Malik & Blue Pyle
+ * @version 3/24/2026
+ */
+public class MinesweeperTwoDimensions extends JPanel implements ActionListener {
+
+    /** The list of numbers for the game */
+    private ArrayList<Integer> nums;
+
+    /** The array of buttons for the game */
+    private MinesweeperButtons[] buttons;
+
+    /** The label for the top of the window */
+    private JLabel topLabel;
+
+    /** The button to start a new game */
+    private JButton newGame;
+
+    /** The button to reset the game */
+    private JButton reset;
+
+    /** The round number for the game */
+    private int roundNum;
+
+    /** The number of rows for the game */
+    private int rows;
+
+    /** The number of columns for the game */
+    private int cols;
+
+    /** The size of the grid for the game (amount of buttons) */
+    private int gridSize;
+
+    /** The CardLayout for managing panels */
+    private CardLayout cardLayout;
+
+    /** The JPanel that holds the cards */
+    private JPanel cards;
+
+    /**
+     * Constructor for the ConcentrationBonus class that initializes the game with
+     * the given parameters.
+     * 
+     * @param rows       the number of rows for the game
+     * @param cols       the number of columns for the game
+     * @param cardLayout the CardLayout for managing panels
+     * @param cards      the JPanel that holds the cards
+     */
+    public MinesweeperTwoDimensions(int rows, int cols, CardLayout cardLayout, JPanel cards) {
+        setLayout(new BorderLayout());
+        this.cardLayout = cardLayout;
+        this.cards = cards;
+
+        this.rows = rows;
+        this.cols = cols;
+
+        gridSize = rows * cols;
+
+        build();
+        fullReset();
+    }
+
+    /**
+     * Private method to build the GUI for the game since run isn't used anymore
+     */
+    private void build() {
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+
+        JPanel topText = new JPanel(new FlowLayout());
+
+        topLabel = new JLabel("Welcome to Minesweeper!");
+        updateRoundTitleText();
+        topText.add(topLabel);
+
+        mainPanel.add(topText, BorderLayout.NORTH);
+        JPanel gamePanel = new JPanel(new GridLayout(rows, cols));
+
+        JPanel bottomButtons = new JPanel(new FlowLayout());
+        newGame = new JButton("New Game");
+        reset = new JButton("Reset");
+        newGame.addActionListener(this);
+        reset.addActionListener(this);
+
+        bottomButtons.add(newGame);
+        bottomButtons.add(reset);
+
+        mainPanel.add(bottomButtons, BorderLayout.SOUTH);
+
+        nums = new ArrayList<Integer>();
+        Random rand = new Random();
+
+        buttons = new MinesweeperButtons[gridSize];
+        for (int i = 0; i < gridSize; i++) {
+            // TODO: CHANGE LOGIC TO ACTUALLY REFLECT
+            if (rand.nextInt(0, 2) == 0) {
+                buttons[i] = new MinesweeperButtons("!!", i);
+                buttons[i].setMine(true);
+            } else {
+                buttons[i] = new MinesweeperButtons(" ", i);
+            }
+            gamePanel.add(buttons[i]);
+            buttons[i].addActionListener(this);
+        }
+        mainPanel.add(gamePanel, BorderLayout.CENTER);
+
+        add(mainPanel);
+
+        setBackground(MainMenu.PRIMARY_COLOR);
+        newGame.setBackground(MainMenu.TERTIARY_COLOR);
+        reset.setBackground(MainMenu.TERTIARY_COLOR);
+        mainPanel.setBackground(MainMenu.SECONDARY_COLOR);
+        bottomButtons.setBackground(MainMenu.SECONDARY_COLOR);
+        topText.setBackground(MainMenu.SECONDARY_COLOR);
+
+    }
+
+    /**
+     * Listens for JButton actions and responds
+     * 
+     * @param e The JButton to listen for
+     */
+    public void actionPerformed(ActionEvent e) {
+
+        // Get the text of the pressed JButton
+        String pressed = e.getActionCommand();
+
+        // Check text of the pressed JButton
+        if (pressed.equals("New Game")) {
+            fullReset();
+            cardLayout.show(cards, "Menu");
+
+        } else if (pressed.equals("Reset")) {
+            reset();
+
+            // Do Logic
+        } else {
+            // TODO: DO THE ACTUAL LOGIC OF THE GAME
+            MinesweeperButtons pressedButton = (MinesweeperButtons) e.getSource();
+            pressedButton.onClick();
+        }
+    }
+
+    /**
+     * Private method to update the round title text at the top of the window
+     * 
+     */
+    private void updateRoundTitleText() {
+        topLabel.setText("Round " + roundNum);
+    }
+
+    /**
+     * Private method to reset the game to the start without changing the numbers
+     */
+    private void reset() {
+        roundNum = 1;
+        updateRoundTitleText();
+
+        for (int i = 0; i < gridSize; i++) {
+            buttons[i].setText(" ");
+            buttons[i].setColor(Color.BLACK);
+            buttons[i].setSelected(false);
+        }
+    }
+
+    /**
+     * Private method to fully reset the game to the start
+     */
+    private void fullReset() {
+        // TODO: ACTUALLY DO A FULL RESET
+        reset();
+
+    }
+
+    /**
+     * Private method to check and print if the game is completed
+     * 
+     */
+    private void onWin() {
+
+        // TODO: ACTUALLY CHECK AND PRINT IF COMPLETED
+
+    }
+}
