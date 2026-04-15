1a9a1c4bfcab545e4b7efcd500fb3bc0142405cc
Patrick K <pp30kosm@siena.edu>
2026-04-15 09:00:30 -0400

    Zero spreading, generalizing

--- Minesweeper/MinesweeperBase.java
+++ Minesweeper/MinesweeperBase.java
@@ -1,6 +1,9 @@
 package Minesweeper;
 
+import java.awt.event.MouseEvent;
+
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 public abstract class MinesweeperBase extends JPanel {
     /**
@@ -9,4 +12,16 @@ public abstract class MinesweeperBase extends JPanel {
      * @return the list of buttons adjacent to one
      */
     public abstract MinesweeperButton[] getAdjacentButtons(int[] position);
+    
+    public void onTileClick(MouseEvent e){
+        MinesweeperButton pressedButton = (MinesweeperButton)e.getSource();
+        if (SwingUtilities.isRightMouseButton(e)){
+            //Right click to flag
+            pressedButton.toggleFlagged();
+        }
+        else{
+            //Normal event
+            pressedButton.reveal();
+        }
+    }
 }
