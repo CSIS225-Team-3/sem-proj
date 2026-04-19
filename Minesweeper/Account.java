73d258a608260f39464036dcf344935207b65845
AhyaanMalik <ak27mali@siena.edu>
2026-04-19 17:32:40 -0400

    basic implementation completed, stores username and a *supposed* hashed password that can be saved (Serializable)

--- Minesweeper/Account.java
+++ Minesweeper/Account.java
@@ -8,5 +8,20 @@ import java.io.Serializable;
  * @version 4/19/2026
  */
 public class Account implements Serializable {
+
+    private final String username;
+    private final String passwordHashed;
     
+    public Account(String username, String passwordHashed) {
+        this.username = username;
+        this.passwordHashed = passwordHashed;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHashed;
+    }
 }
