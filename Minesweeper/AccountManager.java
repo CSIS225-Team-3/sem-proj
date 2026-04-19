7d3aa02be984d090ea3884f773166d15408627fe
AhyaanMalik <ak27mali@siena.edu>
2026-04-19 17:55:52 -0400

    Starting AccountManager, will use SHA-256 to hash passwords.

---
+++ Minesweeper/AccountManager.java
@@ -0,0 +1,13 @@
+package Minesweeper;
+
+import java.security.MessageDigest;
+/**
+ * Account Manager for all Accounts
+ * Will use SHA-256 since it hasn't been broken yet
+ * 
+ * @author Ahyaan Malik (so far)
+ * @version 4/19/2026
+ */
+public class AccountManager {
+    
+}
