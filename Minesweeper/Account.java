637f80c1279adfa7b3af52854f40637c3c33cbd3
AhyaanMalik <ak27mali@siena.edu>
2026-04-19 17:28:47 -0400

    Working on a simple account system (wish me luck)

---
+++ Minesweeper/Account.java
@@ -0,0 +1,12 @@
+package Minesweeper;
+import java.io.Serializable;
+
+/**
+ * Simple Account System for Leaderboards
+ * 
+ * @author Ahyaan Malik (so far)
+ * @version 4/19/2026
+ */
+public class Account implements Serializable {
+    
+}
