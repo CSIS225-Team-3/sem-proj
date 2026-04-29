1b96ca5601c4a3ec87f2221df84c13b1aea457a4
AhyaanMalik <ak27mali@siena.edu>
2026-04-29 09:03:19 -0400

    working on Animated Bar thread class

---
+++ Password_Strength_Checker/AnimatedBar.java
@@ -0,0 +1,19 @@
+
+/**
+ * Animates the bar using threads for Password Checker
+ * 
+ * @author Ahyaan Malik
+ */
+public class AnimatedBar extends Thread {
+
+    private int target;
+
+    public AnimatedBar(int target) {
+        this.target = target;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
