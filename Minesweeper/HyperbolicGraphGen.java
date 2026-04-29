9553292f6163ba70267acd32f77f2a319a933695
Patrick K <pp30kosm@siena.edu>
2026-04-29 16:55:01 -0400

    Added graph gen logic build on hope and prayer

--- Minesweeper/HyperbolicGraphGen.java
+++ Minesweeper/HyperbolicGraphGen.java
@@ -16,18 +16,18 @@ public class HyperbolicGraphGen
         var v3 = e1.r;
         Tile tile0 = new Tile(new Vert[] { v0, v1, v2, v3 });
         tiles.add(tile0);
-        v0.addNeighbor(v1, tile0); //Bottom edge
-        v0.markBuilder(v2, tile0); //Left edge
-        v1.markBuilder(v3, tile0); //Right edge
+        v0.addNeighbor(v1, tile0, Direction.Down); //Bottom edge
+        v0.markBuilder(v2, tile0, Direction.Left); //Left edge
+        v1.markBuilder(v3, tile0, Direction.Right); //Right edge
         
         ExtrudeReturn e2 = extrude3L(v2, v3); //2nd tile
         var v4 = e2.l;
         var v5 = e2.r;
         Tile tile1 = new Tile(new Vert[] { v2, v3, v4, v5 });
         tiles.add(tile1);
-        tile0.addNeighbor(tile1);
-        v4.markBuilder(v5, tile1); //Top edge
-        v3.markBuilder(v5, tile1); //Right edge
+        tile0.addNeighbor(tile1, Direction.Up, Direction.Down);
+        v4.markBuilder(v5, tile1, Direction.Up); //Top edge
+        v3.markBuilder(v5, tile1, Direction.Right); //Right edge
 
 
         var l = v2;
@@ -47,14 +47,20 @@ public class HyperbolicGraphGen
         return tiles.toArray(new Tile[0]);
     }
 
-    static ExtrudeReturn extrude3L(Vert parentL, Vert parentR)
+    /**
+     * A simplified version of extrude which is used for the initial setup
+     * @param parentL The left vertex
+     * @param parentR The right vertex
+     * @return The new vertices
+     */
+    private static ExtrudeReturn extrude3L(Vert parentL, Vert parentR)
     {
         //Full extrusion
         var nL = new Vert();
         var nR = new Vert();
-        parentL.addNeighbor(nL, null);
-        parentR.addNeighbor(nR, null);
-        nL.addNeighbor(nR, null);
+        parentL.addNeighbor(nL, null, null);
+        parentR.addNeighbor(nR, null, null);
+        nL.addNeighbor(nR, null, null);
         return new ExtrudeReturn(nL, nR, null);
     }
 
@@ -67,11 +73,12 @@ public class HyperbolicGraphGen
             var nR = new Vert();
 
             Tile newTile = new Tile(new Vert[] { parentL, parentR, nL, nR });
-            newTile.addNeighbor(parentTile);
+            //idk if that ternary is correct
+            parentTile.addNeighbor(newTile, parentL.neighbors.size() == 4 ? Direction.Left : Direction.Up, Direction.Down);
 
-            parentL.addNeighbor(nL, null);
-            parentR.addNeighbor(nR, newTile);
-            nL.addNeighbor(nR, newTile);
+            parentL.addNeighbor(nL, null, null);
+            parentR.addNeighbor(nR, newTile, Direction.Right);
+            nL.addNeighbor(nR, newTile, Direction.Up);
             return new ExtrudeReturn(parentL, nL, newTile);
         }
         else if (parentL.neighbors.size() == 5)
@@ -82,11 +89,12 @@ public class HyperbolicGraphGen
             var nR = new Vert();
             
             Tile newTile = new Tile(new Vert[] { parentL, parentR, nL.getKey(), nR });
-            newTile.addNeighbor(parentTile);
+            //I'm not certain if Left is always the correct direction, but I think it is
+            parentTile.addNeighbor(newTile, Direction.Left, Direction.Down);
 
             parentL.unmarkBuilder(nL.getKey(), newTile);
-            parentR.addNeighbor(nR, newTile);
-            nL.getKey().addNeighbor(nR, null);
+            parentR.addNeighbor(nR, newTile, Direction.Right);
+            nL.getKey().addNeighbor(nR, null, null);
             return new ExtrudeReturn(nL.getKey(), nR, newTile);
         }
         else
@@ -115,7 +123,7 @@ class Vert
 
     public ArrayList<Vert> neighbors = new ArrayList<>();
     //Vert => Tile
-    public HashMap<Vert, Tile> builderCons = null;
+    public HashMap<Vert, BuilderData> builderCons = null;
 
     /** Tiles this vertex is part of */
     public ArrayList<Tile> tiles = new ArrayList<>();
@@ -124,8 +132,9 @@ class Vert
      * Add a neighbor to this vertex
      * @param neighbor The neighboring vertex
      * @param tile If supplied, the edge between this vertex and the neighbor will be marked as a builder edge associated with this tile
+     * @param dir Which side of the tile this edge is on
      */
-    public void addNeighbor(Vert neighbor, Tile tile)
+    public void addNeighbor(Vert neighbor, Tile tile, Direction dir)
     {
         if (neighbors.contains(neighbor) || neighbor.neighbors.contains(this))
             throw new IllegalStateException("Already a neighbor");
@@ -134,24 +143,29 @@ class Vert
         neighbor.neighbors.add(this);
         
         if (tile != null)
-            markBuilder(neighbor, tile);
+            markBuilder(neighbor, tile, dir);
     }
 
     /**
      * Marks an edge with a neighbor as a builder
      * @param neighbor The neighboring vertex
      * @param tile The tile associated with this builder edge
+     * @param dir Which side of the tile this edge is on
      */
-    public void markBuilder(Vert neighbor, Tile tile)
+    public void markBuilder(Vert neighbor, Tile tile, Direction dir)
     {
         if (!neighbors.contains(neighbor))
             throw new IllegalStateException("Not a neighbor");
 
+        BuilderData data = new BuilderData();
+        data.tile = tile;
+        data.dir = dir;
+
         if (builderCons == null) builderCons = new HashMap<>();
-        builderCons.put(neighbor, tile);
+        builderCons.put(neighbor, data);
 
         if (neighbor.builderCons == null) neighbor.builderCons = new HashMap<>();
-        neighbor.builderCons.put(this, tile);
+        neighbor.builderCons.put(this, data);
     }
 
     /**
@@ -167,10 +181,11 @@ class Vert
         if (builderCons == null || neighbor.builderCons == null)
             throw new IllegalStateException("Error state? Or not a builder? idk");
 
-        Tile tile = builderCons.remove(neighbor);
+        BuilderData data = builderCons.remove(neighbor);
         neighbor.builderCons.remove(this);
 
-        tile.addNeighbor(newTile);
+        //The old tile should always be left of the new one since we're going CCW
+        data.tile.addNeighbor(newTile, data.dir, Direction.Left);
 
         if (builderCons.size() == 0)
             builderCons = null;
@@ -182,6 +197,11 @@ class Vert
     {
         tiles.add(tile);
     }
+
+    private class BuilderData {
+        public Tile tile;
+        public Direction dir;
+    }
 }
 
 class Tile
@@ -192,6 +212,11 @@ class Tile
     /** Direct neighbors */
     public ArrayList<Tile> neighbors = new ArrayList<>();
 
+    public Tile relDown;
+    public Tile relLeft;
+    public Tile relRight;
+    public Tile relUp;
+
     public Vert[] vertices;
 
     public Tile(Vert[] vertices){
@@ -200,12 +225,38 @@ class Tile
             v.addTile(this);
     }
 
-    public void addNeighbor(Tile neighbor)
+    /**
+     * Adds a neighbor to this tile
+     * @param neighbor The neighbor to add
+     * @param selfDir Which (relative) side of this tile that neighbor is on
+     * @param neighborsDir Which (relative) side of the neighbor that this tile is on
+     */
+    public void addNeighbor(Tile neighbor, Direction selfDir, Direction neighborsDir)
     {
         if (neighbors.contains(neighbor) || neighbor.neighbors.contains(this))
             throw new IllegalStateException("Already a neighbor");
 
         neighbors.add(neighbor);
         neighbor.neighbors.add(this);
+
+        setRelSide(neighbor, selfDir);
+        neighbor.setRelSide(this, neighborsDir);
     }
+
+    private void setRelSide(Tile neighbor, Direction dir)
+    {
+        if (dir == Direction.Up)
+            relUp = neighbor;
+        else if (dir == Direction.Down)
+            relDown = neighbor;
+        else if (dir == Direction.Left)
+            relLeft = neighbor;
+        else if (dir == Direction.Right)
+            relRight = neighbor;
+        throw new IllegalStateException("Invalid direction");
+    }
+}
+
+enum Direction {
+    Up, Down, Left, Right
 }
