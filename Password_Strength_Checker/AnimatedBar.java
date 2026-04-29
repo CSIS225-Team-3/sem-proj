86cc96caab76f4df34a5857740f7df9135dfcae7
AhyaanMalik <ak27mali@siena.edu>
2026-04-29 09:30:09 -0400

    changed animation to use doubles, and simplified run code

--- Password_Strength_Checker/AnimatedBar.java
+++ Password_Strength_Checker/AnimatedBar.java
@@ -19,26 +19,27 @@ public class AnimatedBar extends Thread {
 
     @Override
     public void run() {
-        while (strengthBar.getValue() != target) {
-            int current = strengthBar.getValue();
-            int diff = target - current;
-            int step = diff / 7;
+        double current = strengthBar.getValue();
+        while ((int) current != target) {
+            double diff = target - current;
+            current += diff / 7.0;
 
-            if (step == 0) {
-                if (diff > 0) {
-                    step = 1;
-                } else {
-                    step = -1;
-                }
-            }
-            final int next = current + step;
+            // if (step == 0) {
+            //     if (diff > 0) {
+            //         step = 1;
+            //     } else {
+            //         step = -1;
+            //     }
+            // }
+            // final int next = current + step;
+            final int next = (int) current;
             SwingUtilities.invokeLater(() -> {
                 strengthBar.setValue(next);
                 strengthBar.setString(String.valueOf(next));
             });
 
             try {
-                Thread.sleep(20);
+                Thread.sleep(16);
             } catch (InterruptedException e) {
                 break;
             }
