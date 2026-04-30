1b3e81f4ecdada91eca2961c6dac9a0abaf25d12
AhyaanMalik <ak27mali@siena.edu>
2026-04-30 16:21:12 -0400

    formatted

--- Minesweeper/LeaderboardManager.java
+++ Minesweeper/LeaderboardManager.java
@@ -2,19 +2,20 @@ package Minesweeper;
 
 public class LeaderboardManager {
     private static LeaderboardManager instance;
- 
-    private static final String SAVE_FILE   = "Minesweeper//leaderboard.dat";
-    private static final int    MAX_ENTRIES = 10;
- 
+
+    private static final String SAVE_FILE = "Minesweeper//leaderboard.dat";
+    private static final int MAX_ENTRIES = 10;
+
     public static final String[] DIFFICULTIES = { "Easy", "Medium", "Hard", "Extreme" };
     public static final String[] MODES = {
-        "2D Minesweeper", "3D Minesweeper", "4D Minesweeper",
-        "5D+ Minesweeper", "Hyperbolic Minesweeper"
+            "2D Minesweeper", "3D Minesweeper", "4D Minesweeper",
+            "5D+ Minesweeper", "Hyperbolic Minesweeper"
     };
 
-    //TODO: figure out a good data structure to store leaderboard, hashmap most likely
+    // TODO: figure out a good data structure to store leaderboard, hashmap most
+    // likely
 
     public LeaderboardManager() {
-        //LOAD DATA
+        // LOAD DATA
     }
 }
