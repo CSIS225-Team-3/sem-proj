18ba7bf10580484751e56128eda3e7878d70b93e
David Andrew <dddavid.andrewww@gmail.com>
2026-05-01 21:58:12 -0400

    javadoc

--- Password_Strength_Checker/AnimatedBar.java
+++ Password_Strength_Checker/AnimatedBar.java
@@ -8,31 +8,28 @@ import javax.swing.SwingUtilities;
  */
 public class AnimatedBar extends Thread {
 
-<<<<<<< Updated upstream
-=======
-    private float target;
->>>>>>> Stashed changes
 
-    /* A varible to store the current target to interpolate the progress values */
+    /** A varible to store the current target to interpolate the progress values */
     private float target;
 
-    /* Integer to increase the divisions in the progress bar values to create smoother transitioning */
+    /** Integer to increase the divisions in the progress bar values to create smoother transitioning */
     final private int smoothnessScale = 20;
 
-    /* A reference to the progressbar component to change its value */
+    /** A reference to the progressbar component to change its value */
     private JProgressBar strengthBar;
 
-    /* Constructor for the Animmated bar, creates a target value for the bar utilizing scaling */
+    /** Constructor for the Animmated bar, creates a target value for the bar utilizing scaling 
+     * 
+     * 
+     * @param target the target value for the animated bar
+     * @param strengthBar the particiular progress bar to update
+    */
     public AnimatedBar(int target, JProgressBar strengthBar) {
-<<<<<<< Updated upstream
         this.target = (float)(target * smoothnessScale);
-=======
-        this.target = (float)(target * 20);
->>>>>>> Stashed changes
         this.strengthBar = strengthBar;
     }
 
-    /* Run function which updates the value of the strength bar */
+    /** Run function which updates the value of the strength bar */
     @Override
     public void run() {
         float current = strengthBar.getValue();
@@ -40,21 +37,10 @@ public class AnimatedBar extends Thread {
             float diff = target - current;
             current += diff / 7.0f;
 
-<<<<<<< Updated upstream
-=======
-            // if (step == 0) {
-            //     if (diff > 0) {
-            //         step = 1;
-            //     } else {
-            //         step = -1;
-            //     }
-            // }
-            // final int next = current + step;
->>>>>>> Stashed changes
             final float next = current;
             SwingUtilities.invokeLater(() -> {
                 strengthBar.setValue((int)(next));
-                strengthBar.setString(String.valueOf((int)(next0)));
+                strengthBar.setString(String.valueOf((int)(next/smoothnessScale)));
             });
 
             try {
