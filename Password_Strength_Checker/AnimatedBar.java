a08b2861879b5cfb042a97a4cbfe64f11ead64f3
David Andrew <dddavid.andrewww@gmail.com>
2026-05-01 21:35:30 -0400

    javadoc

--- Password_Strength_Checker/AnimatedBar.java
+++ Password_Strength_Checker/AnimatedBar.java
@@ -8,15 +8,23 @@ import javax.swing.SwingUtilities;
  */
 public class AnimatedBar extends Thread {
 
+
+    /* A varible to store the current target to interpolate the progress values */
     private float target;
 
+    /* Integer to increase the divisions in the progress bar values to create smoother transitioning */
+    final private int smoothnessScale = 20;
+
+    /* A reference to the progressbar component to change its value */
     private JProgressBar strengthBar;
 
+    /* Constructor for the Animmated bar, creates a target value for the bar utilizing scaling */
     public AnimatedBar(int target, JProgressBar strengthBar) {
-        this.target = (float)(target * 20);
+        this.target = (float)(target * smoothnessScale);
         this.strengthBar = strengthBar;
     }
 
+    /* Run function which updates the value of the strength bar */
     @Override
     public void run() {
         float current = strengthBar.getValue();
@@ -24,14 +32,6 @@ public class AnimatedBar extends Thread {
             float diff = target - current;
             current += diff / 7.0f;
 
-            // if (step == 0) {
-            //     if (diff > 0) {
-            //         step = 1;
-            //     } else {
-            //         step = -1;
-            //     }
-            // }
-            // final int next = current + step;
             final float next = current;
             SwingUtilities.invokeLater(() -> {
                 strengthBar.setValue((int)(next));
