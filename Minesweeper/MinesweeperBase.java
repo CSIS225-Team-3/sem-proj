d3a7fbcca6b8e51a3c686d55094bfca0f63bd8de
AhyaanMalik <ak27mali@siena.edu>
2026-05-01 21:28:48 -0400

    javadoc

--- Minesweeper/MinesweeperBase.java
+++ Minesweeper/MinesweeperBase.java
@@ -8,18 +8,51 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
+/**
+ * The base class for all Minesweeper game panels, containing shared logic for
+ * handling game over conditions, timers, and leaderboard management.
+ * 
+ * @version 4/30/2026
+ * @author Ahyaan Malik & Patrick Kosmider
+ */
 public abstract class MinesweeperBase extends JPanel {
+    /** Indicates whether the game is over */
     private boolean gameOver = false;
 
+    /** The timer for the game */
     protected Timer timer;
+
+    /** The number of seconds elapsed in the game */
     protected int secondsElapsed;
+
+    /** The label for displaying the timer */
     protected JLabel timerLabel;
 
+    /** The account associated with the leaderboard entry for this game */
     protected Account leaderboardAccount;
+
+    /** The difficulty for the leaderboard entry for this game */
     protected String leaderboardDifficulty;
+
+    /** The mode for the leaderboard entry for this game */
     protected String leaderboardMode;
+
+    /** The leaderboard manager for managing leaderboard entries */
     protected LeaderboardManager leaderboardManager;
 
+    /**
+     * Constructs a new MinesweeperBase with the specified parameters for
+     * leaderboard
+     * 
+     * @param leaderboardManager the leaderboard manager to use for managing
+     *                           leaderboard entries
+     * @param account            the account to associate with the leaderboard entry
+     *                           for this game
+     * @param difficulty         the difficulty to associate with the leaderboard
+     *                           entry for this game
+     * @param mode               the mode to associate with the leaderboard entry
+     *                           for this game
+     */
     public void setLeaderboardInfo(LeaderboardManager leaderboardManager, Account account, String difficulty,
             String mode) {
         this.leaderboardManager = leaderboardManager;
@@ -28,10 +61,10 @@ public abstract class MinesweeperBase extends JPanel {
         this.leaderboardMode = mode;
     }
 
-    protected boolean isEligibleForLeaderboard() {
-        return true;
-    }
-
+    /**
+     * Resets the game by setting gameOver to false and resetting any necessary game
+     * state. Should be called after a win or loss.
+     */
     public void reset() {
         gameOver = false;
     }
@@ -48,7 +81,7 @@ public abstract class MinesweeperBase extends JPanel {
 
             String message = "You win! Congratulations!\nTime spent: " + timerLabel.getText();
 
-            if (isEligibleForLeaderboard() && leaderboardManager != null
+            if (leaderboardManager != null
                     && leaderboardAccount != null && leaderboardDifficulty != null) {
                 boolean madeBoard = leaderboardManager.addEntry(
                         leaderboardMode, leaderboardDifficulty,
